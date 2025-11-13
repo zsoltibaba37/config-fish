f29299f8d6a1b7c9a066b8dbd2e2a9e9dba84b41
Zsolt <petozsolt33@gmail.com>
2025-11-13 17:19:41 +0100

    setup to old version

--- config.fish
+++ config.fish
@@ -5,36 +5,37 @@ if status is-interactive
     alias free="free -h"
     alias df="df -h"
     set fish_greeting
+    
 end
 
 function fish_prompt
 
   echo ""
-  set_color green
 
-  echo -n (whoami)
-
-  set_color normal
-
-  echo -n "@"
-  echo -n (hostname)
-  echo -n " "
-
-  set_color green
-  echo -n "[ "
-  echo -n (date +%Y.%m.%d\ -\ %H:%M)
-  echo -n " ]"
-
-  echo -n " - "
   set_color blue
-
+  echo -n "["
   echo -n (pwd)
+  echo -n "]"
 
   set_color normal
+  echo -n " - "
 
+  set_color normal
+  echo -n "["
+  echo -n (date +%Y.%m.%d\ -\ %H:%M)
+  echo -n "]"
+
+  set_color blue
   echo -n (fish_git_prompt)
 
   echo ""
-  echo -n "λ "
+
+  set_color green
+  echo -n (whoami)
+  echo -n "@"
+  echo -n (hostname)
+  echo -n ""
+  set_color normal
+  echo -n ":\$ "
 
 end
